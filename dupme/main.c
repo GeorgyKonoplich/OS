bb80a3db7d28723deed9fd0ccbd1976e975860da
j0r1k <j0r1k@j0r1k.(none)>
2013-04-04 07:32:10 +0400

    final

---
+++ dupme/main.c
@@ -0,0 +1,40 @@
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char** argv){
+	int input, output, m, error, i;
+	int len = 0;
+	int k = atoi(argv[1]);
+	char* buf = (char*) malloc(k + 2);
+	while(1){
+		input = read(0, buf + len, k - len + 1);
+		if (input >= 0) len =  len + input;
+		if (input == 0 && len == 0){
+			break;
+		} else if (input == 0){
+			buf[len] = '\n';
+			len = len + 1;
+		}
+		m = -1;
+		for (i = 0; i < len; i++) if (buf[i] == '\n'){
+			if (error == 1){
+				error = 0;
+				m = i; 
+				break;
+			}
+			output = write(1, buf + m + 1, i - m);
+			output = write(1, buf + m + 1, i - m);
+			m = i;
+		}
+		if (m == -1){
+			len = 0;
+			error = 1;
+		}else{ 
+			len = len - m - 1;
+			memmove(buf, buf + m + 1, len);
+		}
+	}
+	free(buf);
+	return 0;
+}
+
